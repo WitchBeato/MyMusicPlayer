6bb11cd5d475c849f4b212d664db72cde67cf6a7
ali korkmaz <alisonkorkmaz@gmail.com>
2024-07-17 03:20:28 +0300

    new images added for dropdown menu

--- src/directories/Imagedtr.java
+++ src/directories/Imagedtr.java
@@ -9,4 +9,6 @@ public class Imagedtr {
 			soundlvl3 = local+"sound+++.png",soundno = local+"nosound.png";
 	static public String play = local+"continue.png", stop = local+"stop.png",  
 			prev = local+"prev.png", onward = local+"onward.png";
+	static public String arrowup = local+"arrowup.png",arrowdown = local+"arrowdown.png";
+	static public String folder = local+"folder.png", playlist = local+"playlist.png";
 }
